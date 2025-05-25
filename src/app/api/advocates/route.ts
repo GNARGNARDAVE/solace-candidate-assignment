de8a41cfe9380efd3f3e45c7618b3e9a8c523a8c
DT <dtran@remine.com>
2025-05-25 15:13:38 -0400

    add-enchancements: Add like to advocate route

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -1,19 +1,27 @@
+import { and, asc, desc, ilike, SQL } from 'drizzle-orm';
+import { NextRequest } from 'next/server';
 import db from '@/db';
 import { advocates } from '@/db/schema';
-import { asc, desc } from 'drizzle-orm';
-import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
     const searchParams = request.nextUrl.searchParams;
     const key = searchParams.get('key');
     const sort = searchParams.get('sort');
+    const searchKey = searchParams.get('searchKey');
+    const searchInput = searchParams.get('searchInput');
 
     let orderBy = null;
     if (key && sort) {
         orderBy = sort === 'ASC' ? asc(advocates[key]) : desc(advocates[key]);
     }
-
-    const data = await db.select().from(advocates).orderBy(orderBy);
+    const filters: SQL[] = [];
+    if (searchKey) {
+        filters.push(ilike(advocates['searchKey'], searchInput));
+    }
+    const data = await db.select()
+        .from(advocates)
+        .where(and(...filters))
+        .orderBy(orderBy);
 
     return Response.json(data);
 }
