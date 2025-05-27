f2057a4d5953c42fd0e334e94dcb09078187240e
DT <dtran@remine.com>
2025-05-27 08:17:22 -0400

    add-enhancements: Remove console info

--- src/app/api/advocates/route.ts
+++ src/app/api/advocates/route.ts
@@ -24,7 +24,6 @@ export async function GET(request: NextRequest) {
         .from(advocates)
         .where(and(...filters))
         .orderBy(orderBy);
-
-    console.info('data', data);
+    
     return Response.json(data);
 }
